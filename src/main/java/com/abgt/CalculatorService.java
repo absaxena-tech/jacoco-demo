e8d0307a843e1b548a2829b0260fd1828603163f
Abhijeet Saxena <absaxena@griddynamics.com>
2026-03-11 11:00:13 +0530

    jacoco code initial commit

--- src/main/java/com/abgt/CalculatorService.java
+++ src/main/java/com/abgt/CalculatorService.java
@@ -1,4 +1,17 @@
 package com.abgt;
 
 public class CalculatorService {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+
+        return a / b;
+    }
 }
--- src/test/java/com/abgt/CalculatorServiceTest.java
+++ src/test/java/com/abgt/CalculatorServiceTest.java
@@ -1,4 +1,32 @@
 package com.abgt;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorServiceTest {
+
+    CalculatorService calculator = new CalculatorService();
+
+    @Test
+    void testAdd() {
+        int result = calculator.add(5, 3);
+        assertEquals(8, result);
+    }
+
+    @Test
+    void testDivide() {
+        int result = calculator.divide(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testDivideByZero() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.divide(10, 0)
+        );
+
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
 }
